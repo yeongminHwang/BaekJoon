88b8030209b5c1933f97bf1a816672b3671063d7
yeongminHwang <hwongyoungmin@gmail.com>
2024-08-24 00:05:04 +0900

    Ex_10813

--- src/Ex_10813.java
+++ src/Ex_10813.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Ex_10813 {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    static int[] bucket;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        bucket = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            bucket[i] = i;
+        }
+        for (int k = 1; k <= M; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            swap(i, j);
+        }
+        for (int i = 1; i <= N; i++) {
+            bw.write(bucket[i] + " ");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static void swap(int i, int j) {
+        int temp = bucket[i];
+        bucket[i] = bucket[j];
+        bucket[j] = temp;
     }
 }
